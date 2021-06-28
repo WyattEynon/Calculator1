ce14e06861db18b80a795f1bc49c86ea07bf43e6
Wyatt Eynon <wme4@njit.edu>
2021-06-27 21:13:32 -0400

    Added in first custom CSV test of my own creation Task: Added unit test csvs

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,6 +1,6 @@
 import unittest
 from Calculator import Calculator
-#from CsvReader import CsvReader
+from CsvReader import CsvReader
 from pprint import pprint
 #Sourced from example file
 
@@ -10,12 +10,16 @@ class MyTestCase(unittest.TestCase):
         calculator=Calculator()
         self.assertIsInstance(self.calculator, Calculator)
 
-    #def setUp(self) -> None:
-        #self.calculator = Calculator()
-    #def test_subtraction(self):
-        #test_data = CsvReader('/src/subtraction.csv').data
-        #for row in test_data:
-            ##self.assertEqual(self.calculator.result, int(row['Result']))
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+    def test_subtraction(self):
+        test_data = CsvReader('/src/csvTests/Unit Test Subtraction.csv').data
+        for row in test_data:
+            myCalc = Calculator()
+            myResult = myCalc.subtract((int(row['Value 1'])),(int(row['Value 2'])))
+            self.assertEqual(((int(row['Result']))),(myResult))
+
+            #self.assertEqual(self.calculator.result, int(row['Result']))
     #def test_instantiate_calculator(self):
         #self.assertIsInstance(self.calculator, Calculator)
     #def test_results_property(self):
