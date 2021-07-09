50c81dacf4ccaed0f007d5c658169e61eb16d582
Wyatt Eynon <wme4@njit.edu>
2021-07-08 21:39:01 -0400

    Added rudimentary input checking mechanism

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,24 +1,45 @@
 class Calculator:
 
-    result = 0
-
     def __init__(self):
         pass
+    result = 0
+    #Input Regualtion
+    #Operations Below
+
+
     def add(self, a, b):
-        self.result = a + b
-        return self.result
+        if ((type(a) is int) or (type(a) is float)) and ((type(b) is int) or (type(b) is float)):
+            self.result = a + b
+            return self.result
+        else:
+            print("Not valid input")
     def subtract(self, b, a):
-        self.result = a-b
-        return self.result
+        if ((type(a) is int) or (type(a) is float)) and ((type(b) is int) or (type(b) is float)):
+            self.result = a-b
+            return self.result
+        else:
+            print("Not valid input")
     def square(self, a):
-        self.result = a*a
-        return self.result
+        if ((type(a) is int) or (type(a) is float)) and ((type(b) is int) or (type(b) is float)):
+            self.result = a*a
+            return self.result
+        else:
+            print("Not valid input")
     def sqrt(self, a):
-        self.result = a ** 0.5
-        return self.result
+        if ((type(a) is int) or (type(a) is float)):
+            self.result = a ** 0.5
+            return self.result
+        else:
+            print("Not valid input")
     def multiply(self, a, b):
-        self.result =a * b
-        return self.result
+        if ((type(a) is int) or (type(a) is float)):
+            self.result =a * b
+            return self.result
+        else:
+            print("Not valid input")
     def divide(self, a, b):
-        self.result = b / a
-        return self.result
+        if ((type(a) is int) or (type(a) is float)):
+            self.result = b / a
+            return self.result
+        else:
+            print("Not valid input")
